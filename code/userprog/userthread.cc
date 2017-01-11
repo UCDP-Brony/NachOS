63371db00996de232aa59d15c40e4f026ed8d1ed
Neliron <neliron@Neliron.neliron>
2017-01-11 15:08:29 +0100

    Tried to fix the 'Address error' problem, but didn't work

--- code/userprog/userthread.cc
+++ code/userprog/userthread.cc
@@ -2,27 +2,42 @@
 #include "userthread.h"
 #include "syscall.h"
 #include "system.h"
+#include "synch.h"
 
 typedef struct ThreadArgs{
 	int function;
 	int arg;
 }ThreadArgs;
 
+int compteur = 0;
+Semaphore *starting = new Semaphore("Starting Thread", 1);
+
 static void StartUserThread(int f){
 
+	starting->P();
+	compteur++;
+	DEBUG('a', "Before InitRegisters");
 	currentThread->space->InitRegisters();
+	DEBUG('a', "Before RestoreRegister");
 	currentThread->space->RestoreState();
-	ThreadArgs * params = (ThreadArgs *) f;
+
+	printf("Compteur : %i", compteur);
 
 	int spr = machine->ReadRegister (StackReg);
+	machine->WriteRegister (StackReg, spr - ((PageSize*2)*compteur));
 
-	//printf("args = %i\n", params->arg);
+	ThreadArgs * params = (ThreadArgs *)f;
 
+	// int* pvalue = (int*)params->arg;
+	// printf("args = %x\n", *pvalue);
+
+	machine->WriteRegister (RetAddrReg, PCReg+24);
+	DEBUG('a', "Before WriteRegister");
 	machine->WriteRegister (PCReg, params->function);
 	machine->WriteRegister (NextPCReg, params->function + 4);
 	machine->WriteRegister (4, params->arg);
-	machine->WriteRegister (StackReg, spr - (PageSize*2));
-	machine->WriteRegister (RetAddrReg, (int)do_UserThreadExit);
+	DEBUG('a',"Before Run");
+	starting->V();
 	machine->Run();
 }
 
@@ -30,21 +45,24 @@ int do_UserThreadCreate(int f, int arg){
 	ThreadArgs args;
 	args.function = f;
 	args.arg = arg;
+	printf("Argument : %i", args.arg);
 	Thread *thread = new Thread("thread utilisateur");
 	//printf("DEBUG 1\n");
 	thread->Fork(StartUserThread, (int)&args);
 	//currentThread->Yield();
-	//interrupt->SetLevel(IntOff);
-	//currentThread->Sleep();
+	interrupt->SetLevel(IntOff);
+	currentThread->Sleep(); // if this is not commented then the first thread do what it is supposed to do, otherwise the adresses used later are corrupted
 	//printf("DEBUG 5\n");
 
 	return 0;
 }
 
 void do_UserThreadJoin(){
-	while(scheduler->FindNextToRun() != NULL){
+	/*while(scheduler->FindNextToRun() != NULL){
 		currentThread->Yield();
-	}
+	}*/
+	interrupt->SetLevel(IntOff);
+	currentThread->Sleep();
 }
 
 void do_UserThreadExit(){
