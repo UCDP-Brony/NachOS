a6399f99cf4d7fdb017b1ee2acab4855d1c757e0
zoey <i1684746@trbvm.com>
2017-01-19 17:02:22 +0100

    added random page

--- code/userprog/frameprovider.cc
+++ code/userprog/frameprovider.cc
@@ -1,8 +1,11 @@
 #include "frameprovider.h"
 #include "system.h"
 
+bool frameRandom;
+
 FrameProvider::FrameProvider(int nbFrames){
 	bMap = new BitMap(nbFrames);
+	frameRandom = true;
 }
 
 FrameProvider::~FrameProvider(){
@@ -10,7 +13,16 @@ FrameProvider::~FrameProvider(){
 }
 
 unsigned int FrameProvider::GetEmptyFrame(){
-	unsigned int frameIndex = bMap->Find();
+	unsigned int frameIndex;
+	if(frameRandom){ 
+		//random
+		frameIndex = bMap->RandomFind();
+	}
+	else 
+	{
+		//vanilla
+		frameIndex = bMap->Find();
+	}
 	if((int)frameIndex != -1){
 		bzero( machine->mainMemory + (frameIndex*PageSize), PageSize);
 	}
@@ -21,4 +33,9 @@ void FrameProvider::ReleaseFrame(unsigned int frame){
 }
 int FrameProvider::NumAvailFrame(){
 	return bMap->NumClear();
+}
+
+//possiblity to enable/disable random mode
+void setFrameRandom(bool b){
+	frameRandom = b;
 }
